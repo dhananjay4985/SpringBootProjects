e5303f99e77fc1b832d8fbc480473bdccd4fd907
dshinde <dhananjay4985@gmail.com>
2018-09-24 00:05:22 +0530

    methods defined in note interface

--- UserNoteWithCloudOAuth2.0/src/main/java/user/note/usernotewithcloudoauth2/service/NoteService.java
+++ UserNoteWithCloudOAuth2.0/src/main/java/user/note/usernotewithcloudoauth2/service/NoteService.java
@@ -1,5 +1,15 @@
 package user.note.usernotewithcloudoauth2.service;
 
+import java.util.List;
+
+import user.note.usernotewithcloudoauth2.model.Note;
+
 public interface NoteService {
 
+	Note getNoteById(Long noteId);
+	void createNote(Note note);
+	void updateNote(Note Note);
+	void deleteNoteById(Long noteId);
+	Note getNoteByTitle(String title);
+	List<Note> getAllNote();
 }
