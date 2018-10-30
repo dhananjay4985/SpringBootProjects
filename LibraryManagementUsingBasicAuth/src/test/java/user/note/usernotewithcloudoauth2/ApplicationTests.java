e37fc7d4af20bf927580091a1fd4f14fca145990
Dhananjay,Shinde <dhananjay4985@gmail.com>
2018-10-30 16:18:22 +0530

    comment

--- LibraryManagementUsingBasicAuth/src/test/java/user/note/usernotewithcloudoauth2/ApplicationTests.java
+++ LibraryManagementUsingBasicAuth/src/test/java/user/note/usernotewithcloudoauth2/ApplicationTests.java
@@ -1,16 +1,66 @@
 package user.note.usernotewithcloudoauth2;
 
+import java.io.UnsupportedEncodingException;
+
+import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import user.library.librarymanagementusingbasicauth.model.Book;
+import user.library.librarymanagementusingbasicauth.service.BookService;
+import user.library.librarymanagementusingbasicauth.service.UserService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=ApplicationTests.class)
+@AutoConfigureMockMvc
 public class ApplicationTests {
-
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private BookService bookService;
+	
+	@MockBean
+	private UserService userService;
+	
+	
+	Book mockBook = new Book(123456L,"Java8","Oracle","dfdfd","sdfsdfsdf");
+	
+	String exampleJSON = "{\"bookId\":\"123456L\",\"bookName\":\"Java8\",\"author\":\"Oracle\",\"genre\":\"dfdfd\",\"isbn\":\"sdfsdfsdf\"}"; 
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void retrieveBookDetails() throws Exception {
+		Mockito.when(bookService.findById(Mockito.anyLong())).thenReturn(mockBook);
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/allbooks/123456L").accept(MediaType.APPLICATION_ATOM_XML.APPLICATION_JSON);
+					
+		//ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/allbooks/123456L").accept(MediaType.APPLICATION_ATOM_XML.APPLICATION_JSON));
+		//resultActions.andDo(MockMvcResultHandlers.print());
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		
+		String expected = "{bookId:123456L,bookName:Java8,author:Oracle,genre:dfdfd,isbn:sdfsdfsdf}";
+		System.out.println("Hii "+result.getResponse().getContentAsString());
+		JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+		
 	}
 
 }
