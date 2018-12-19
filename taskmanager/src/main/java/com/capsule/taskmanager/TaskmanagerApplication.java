9c03143877e3d6f76e0254100604e9f6bd715b9a
Dhananjay,Shinde <dhananjay4985@gmail.com>
2018-12-19 13:16:02 +0530

    added corsConfigurer

--- taskmanager/src/main/java/com/capsule/taskmanager/TaskmanagerApplication.java
+++ taskmanager/src/main/java/com/capsule/taskmanager/TaskmanagerApplication.java
@@ -2,6 +2,10 @@ package com.capsule.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class TaskmanagerApplication {
@@ -9,4 +13,18 @@ public class TaskmanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                        .allowedHeaders("header1", "header2") //What is this for?
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
